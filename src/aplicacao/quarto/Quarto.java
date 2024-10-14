4822bc5959e1773e658aaec10871d4099d8c3efe
Jaun-22 <joaopgl2000@gmail.com>
2024-10-14 18:00:00 -0300

    - Criada lógica para avaliação de desempenho.

--- src/aplicacao/quarto/Quarto.java
+++ src/aplicacao/quarto/Quarto.java
@@ -1,7 +1,21 @@
 package aplicacao.quarto;
 
 public class Quarto {
-    public void quartoExecicio() {
-        String nome = "Joao";
+    public void quartoExercicio() {
+        //EXERCÍCIO 2- AVALIAÇÃO DE DESEMPENHO
+
+        int nota = 100;
+        if (nota > 90) {
+            System.out.println("Sua nota foi excelente: " + nota);
+        } else if (nota > 80) {
+            System.out.println("Sua nota foi ótimo: " + nota);
+        } else if (nota > 70) {
+            System.out.println("Sua nota foi boa:" + nota);
+        } else if (nota > 60) {
+            System.out.println("Sua nota foi suficiente:" + nota);
+        } else {
+            System.out.println("sua nota foi insuficiente");
+        }
+
     }
 }
