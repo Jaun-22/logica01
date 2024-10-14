2f316ef0a09d86edd75f5aa07374e6281cf10229
Jaun-22 <joaopgl2000@gmail.com>
2024-10-14 18:01:26 -0300

    - Criada lógica para verificação de letras "vogal" ou "Consoante".

--- src/aplicacao/terceiro/Terceiro.java
+++ src/aplicacao/terceiro/Terceiro.java
@@ -3,5 +3,14 @@ package aplicacao.terceiro;
 public class Terceiro {
     public void terceiroExecicio() {
 
+        String letra = "a";
+
+        if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u"
+                || letra == "A" || letra == "E" || letra == "I" || letra == "O" || letra == "U") {
+            System.out.println("A letra: " + "'" + letra + "'" + " é uma vogal");
+        } else {
+            System.out.println("A letra: " + "'" + letra + "'" + " é uma consoante");
+        }
+
     }
 }
