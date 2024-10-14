0a05fe516b818e14b5c3a9376e8ea825e2e24553
Jaun-22 <joaopgl2000@gmail.com>
2024-10-14 17:56:57 -0300

    - Instanciado classe Quarto e realizado chamada do metodo quartoExercicio.
    - instanciado Terceiro e realizado chamada do metodo terceiroExercicio.

--- src/aplicacao/Aplicacao.java
+++ src/aplicacao/Aplicacao.java
@@ -1,11 +1,25 @@
 package aplicacao;
 
 import aplicacao.primeiro.Primeiro;
+import aplicacao.quarto.Quarto;
+import aplicacao.segundo.Segundo;
+import aplicacao.terceiro.Terceiro;
 
 public class Aplicacao {
     public static void main(String[] args) {
 
         Primeiro instancia = new Primeiro();
         instancia.primeiroExecicio();
+        instancia = new Primeiro();
+
+        Quarto quartaAtividade = new Quarto();
+        quartaAtividade.quartoExercicio();
+        quartaAtividade = new Quarto();
+
+        Terceiro terceiraAtivdade = new Terceiro();
+        terceiraAtivdade.terceiroExecicio();
+        terceiraAtivdade = new Terceiro();
+
+
     }
 }
