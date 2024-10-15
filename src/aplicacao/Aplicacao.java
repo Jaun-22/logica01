d0ed15a3bd3cda3dd50b074451c0ad50dfbf6b85
Jaun-22 <joaopgl2000@gmail.com>
2024-10-15 18:59:11 -0300

    - Instanciado aula primeiro e realizada chamada do método primeiroExercicio.
    - Instanciado aula segundo e realizada chamada do método segundoExercicio.
    
    - Instanciado aula primeiro e realizada chamada do método primeiroExercicio.
    
    - Instanciado aula terceiro e realizada chamada do método teceiroExercicio.
    
    - Instanciado aula primeiro e realizada chamada do método primeiroExercicio.

--- src/aplicacao/Aplicacao.java
+++ src/aplicacao/Aplicacao.java
@@ -8,18 +8,24 @@ import aplicacao.terceiro.Terceiro;
 public class Aplicacao {
     public static void main(String[] args) {
 
+
         Primeiro instancia = new Primeiro();
         instancia.primeiroExecicio();
         instancia = new Primeiro();
 
-        Quarto quartaAtividade = new Quarto();
-        quartaAtividade.quartoExercicio();
-        quartaAtividade = new Quarto();
+        Segundo segundaAtividade = new Segundo();
+        segundaAtividade.segundoExecicio();
+        segundaAtividade = new Segundo();
+
 
         Terceiro terceiraAtivdade = new Terceiro();
         terceiraAtivdade.terceiroExecicio();
         terceiraAtivdade = new Terceiro();
 
+
+        Quarto quartaAtividade = new Quarto();
+        quartaAtividade.quartoExercicio();
+        quartaAtividade = new Quarto();
 
     }
 }
