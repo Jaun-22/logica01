0481b7e10e23ff14e9fb4404e073562a9784ecf8
Jaun-22 <joaopgl2000@gmail.com>
2024-10-15 18:50:18 -0300

    - Criada lógica para verificação da média,
    /*
     * Este programa calcula a média de quatro notas fornecidas pelo usuário.
     * O usuário é solicitado a inserir quatro valores decimais (notas).
     * O cálculo da média é feito somando as quatro notas e dividindo o resultado por 4.
     * Após o cálculo, o programa verifica se a média é maior ou igual a 7.0.
     * Se a média for maior ou igual a 7.0, o programa informa que a nota está na média.
     * Caso contrário, informa que a nota está abaixo da média.
     * O valor da média é exibido com duas casas decimais.
     */

--- src/aplicacao/segundo/Segundo.java
+++ src/aplicacao/segundo/Segundo.java
@@ -1,7 +1,34 @@
 package aplicacao.segundo;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Segundo {
     public void segundoExecicio() {
+        Scanner valorMedia = new Scanner(System.in);
+
+        System.out.println("Digite sua primeira nota: ");
+        double notaUm = valorMedia.nextDouble();
+
+        System.out.println("Digite sua segunda nota: ");
+        double notaDois = valorMedia.nextDouble();
+
+        System.out.println("Digite sua terceira nota: ");
+        double notaTres = valorMedia.nextDouble();
+
+        System.out.println("Digite sua quarta nota: ");
+        double notaQuatro = valorMedia.nextDouble();
+
+
+        double media = (notaUm + notaDois + notaTres + notaQuatro) / 4;
+
+
+        if (media >= 7.0) {
+            System.out.println("Você tirou: " + String.format("%.2f", media) + " Sua nota esta na média.");
+        } else {
+            System.out.println("Você tirou: " + String.format("%.2f", media) + " Sua nota esta a baixo da média.");
+        }
 
     }
+
 }
